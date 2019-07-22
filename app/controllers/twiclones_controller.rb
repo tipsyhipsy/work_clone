c7b81ac2b251a3137d806ca67e9873c5cbe16e8e
setsuto horikawa <ralralph@gmail.com>
2019-07-22 19:41:35 +0900

    一覧機能実装

--- app/controllers/twiclones_controller.rb
+++ app/controllers/twiclones_controller.rb
@@ -1,20 +1,21 @@
 class TwiclonesController < ApplicationController
-  def index
-  end
+  # def index
+  # end
 
   def new
+    @twiclones = Twiclone.all
     @twiclone = Twiclone.new
   end
 
   def create
-    Twiclone.create(content:[:content])
+    Twiclone.create(content: params[:twiclone][:content])
     redirect_to '/twiclones/new'
   end
 
   def edit
   end
-
   private
-  def contact_params
-  end
+  # def content_params
+  #   params.require(:twiclone).permit(:content)
+  # end
 end
